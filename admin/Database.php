e96310d8c1c77dadefda9c607e984375a66ea457
unknown <elgrysyoussef78@gmail.com>
2022-11-15 11:33:38 +0100

    mise à jour admin

--- admin/Database.php
+++ admin/Database.php
@@ -3,31 +3,34 @@
 class Database
 {
 
-    $dbHost = "localhost";
-    $dbName = "web_eat";
-    $dbUser = "root";
-    $userPassword = "";
-    
-    
-    
-function connect(){
-      
-try {
-    
-    $connection = new PDO("mysql:host=" . $dbHost . ";dbname=" . $dbName, $dbUser, $userPassword);
-} 
-catch (PDOException $e) 
-{
-    
-    die($e->getMessage());
+    private static $dbHost = "localhost";
+    private static $dbName = "web_eat";
+    private static $dbUser = "root";
+    private static $userPassword = "";
+
+    private static $connection = null;
+
+
+
+    public static function connect()
+    {
+
+        try {
+
+            self::$connection = new PDO("mysql:host=" . self::$dbHost . ";dbname=" . self::$dbName, self::$dbUser, self::$userPassword);
+        } catch (PDOException $e) {
+
+            die($e->getMessage());
+        }
+        return self::$connection;
+    }
+
+    public static function disconnect()
+    {
+        self::$connection = null;
+    }
 }
 
-}
-    
 
-
-
-
-}
 
 ?>
